f583f79a12926db5b4ed0820ed9a41dd9de5ef03
Vinicius Silva <vinicius.silva.89@outlook.com>
2021-07-07 09:10:57 -0300

    Javascript-02
    Material de estudo.

---
+++ JavaScript/Js/funcoes.js
@@ -0,0 +1,71 @@
+window.onload = function () {
+  /*function RealizarConta(num1, num2) {
+    console.log(num1 + num2);
+    console.log(num2);
+
+    if (num1 == 10) {
+      console.log("Meu número 1 é igual a 10");
+    }
+  }
+
+  RealizarConta(10, 20);
+  */
+  /*
+  function RealizarConta(num1, num2) {
+    var nome = "Vini";
+    return nome;
+  }
+
+  var resultado = RealizarConta(10, 10);
+
+  console.log(resultado);
+  */
+  /*
+  var variavel = function () {
+    console.log("Minha função atravéz da variável");
+  };
+  */
+  /*
+  function teste(num1, num2) {
+    return num1 + num2;
+  }
+
+  var variavel = teste(10, 20);
+
+  console.log(typeof variavel);
+  console.log(variavel);
+  */
+  /*
+  var obj = {
+    nome: "Vinicius",
+    idade: 32,
+    func: function () {
+      console.log("Vinicius");
+    },
+  };
+
+  obj.nome = "Vini";
+  console.log(obj.nome);
+  obj.func();
+  */
+  /*
+  var obj = { nome: "Vinicius", Idade: 32 };
+
+  obj.func = function () {
+    console.log("minha função");
+  };
+
+  obj.func();
+  */
+
+  var variavel = [];
+
+  variavel[0] = [];
+
+  variavel[0][2] = "Vinicius";
+  console.log(variavel[0][2]);
+
+  variavel.obj = {};
+  variavel.obj.nome = "Vinis";
+  console.log(variavel.obj.nome);
+};
--- JavaScript/Js/functions.js
+++ JavaScript/Js/functions.js
@@ -1,3 +1,48 @@
 window.onload = function () {
-  alert("Olá Mundo!");
+  // var nome = "Vinicius";
+  // === Além de ser igual a variável tem que ser do mesmo tipo.
+  // ==
+  // >=
+  // <=
+  // != Diferente
+  // !== Diferente valor e tipo
+  // >
+  // <
+  // if (nome == "João") {
+  // alert("Condição verdadeira");
+  // } else if (nome == "Vinicius") {
+  // alert("Condição verdadeira");
+  // } else {
+  // alert("Condição falsa");
+  // var idade = 2021 - 1989;
+  // if (idade > 30) {
+  // console.log("Eu tenho mais de 30 anos de idade!");
+  // } else if (idade < 10) {
+  // console.log("Menor que 10");
+  // } else if (idade < 5) {
+  // console.log("Menor que 5");
+  // } else {
+  // console.log("Nenhuma das condições são verdadeiras");
+  // }
+  // var number = "23";
+  // if (number === 23) {
+  //   console.log("Verdadeiro");
+  // } else {
+  //   console.log("Falso");
+  // }
+  /*var repeticao = 0;
+  var nome = "Vinicius";*/
+  // While: precisamos de uma variável inicial.
+  /*while (nome == "Vinicius") {
+    console.log("Olá mundo");
+    nome = "João";
+
+    console.log("Cheguei até o final! ");
+  }*/
+  for (var numero = 0; numero < 10; numero++) {
+    console.log("Olá mundo!");
+  }
+  /*do {
+    console.log("Olá mundo!!");
+  } while (nome == "Vinicius");*/
 };
